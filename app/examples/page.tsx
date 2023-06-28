6a485a92853f0473d694565c86953f747bb37651
Philip Rothschild <philiprene89@gmail.com>
2023-06-28 20:01:01 +0200

    🔀 refactor(page.tsx): replace ReadExamples component with BananaFunsiesDisplay component to simplify code and improve readability 🔀 refactor(read-examples.tsx): remove ReadExamples component as it is no longer needed and has been replaced by BananaFunsiesDisplay component

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,11 +1,20 @@
+import { db } from "~/db";
 import { NewExample } from "./new-example";
-import { ReadExamples } from "./read-examples";
+import { BananaFunsiesDisplay } from "./banana-funsies-display";
 
 const ProductsPage = async () => {
+  const examples = await db.query.example.findMany({
+    orderBy: (column, sort) => sort.desc(column.id),
+  });
   return (
     <div className="space-y-8 flex flex-col gap-4 justify-center items-center m-2">
-      <NewExample />
-      <ReadExamples />
+      <div key={examples?.[0].id}>
+        <NewExample />
+      </div>
+      <div className="flex flex-col gap-4">
+        <h1>Examples</h1>
+        <BananaFunsiesDisplay examples={examples} />
+      </div>
     </div>
   );
 };
