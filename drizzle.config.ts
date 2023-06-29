83a2051ff937197364cd62e34e6e116db06136ca
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2023-06-29 14:07:40 +0200

    get connection string from .env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,6 @@ export default {
   schema: "./db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: 'mysql://hdxuen9n0j12if6pbpqm:pscale_pw_CjckOkXJqGCCEGFz7vbtHmROuKQ40YvIxqUzwkbCJya@aws.connect.psdb.cloud/nexjs-example?ssl={"rejectUnauthorized":true}',
+    connectionString: process.env.DATABASE_URL as string,
   },
 } satisfies Config;
